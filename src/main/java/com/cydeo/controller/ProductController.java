89d6ea2a72337a3dd95a7d7720e522da3bf4ba49
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-03-30 22:21:55 -0400

    lab-05-mvc

--- src/main/java/com/cydeo/controller/ProductController.java
+++ src/main/java/com/cydeo/controller/ProductController.java
@@ -1,4 +1,22 @@
 package com.cydeo.controller;
 
+
+import com.cydeo.service.impl.ProductServiceImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ProductController {
+
+    @RequestMapping("/search-product/{product}")
+    public String productPage(Model model, @PathVariable String product){
+
+        ProductServiceImpl productService = new ProductServiceImpl();
+
+        model.addAttribute("productList",productService.searchProduct(product));
+
+        return "product/product-list";
+    }
 }
